028ea5323b31dcf9b21bc3e6d88b1356ae4b3650
Naveenkumar <vrnaveenkr@gmail.com>
2020-04-19 19:59:26 +0530

    SED-2 ABCD
    ABCD SED-2

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! MAH-2--d" );
+        System.out.println( "Hello World RefX! SED-2--d" );
     }
 }
+#ABCDEFGH
